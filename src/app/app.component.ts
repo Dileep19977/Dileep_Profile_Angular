68fbca3a6df2ff872b86d818d2f8e05cc3c8fee4
Dileep19977 <dileep.yf52@gmail.com>
2022-11-29 16:19:05 -0600

    New deploy config

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Dileep_Angular_Profile';
+}
